b6cdd4bc8bb6e875c93f8e8dd045ea908afb52dc
matosnegociosimibiliarios <matosaedu@gmail.com>
2026-09-17 13:51:58 -0300

    fix: corrigir permissao da rota financeira

--- src/App.jsx
+++ src/App.jsx
@@ -95,7 +95,7 @@ export default function App() {
           <Route path="/admin/locacoes" element={<PermissionRoute permission="rentals.view"><AdminRentals /></PermissionRoute>} />
           <Route path="/admin/locacoes/nova" element={<PermissionRoute permission="rentals.manage"><AdminRentalForm /></PermissionRoute>} />
           <Route path="/admin/locacoes/:id" element={<PermissionRoute permission="rentals.view"><AdminRentalDetail /></PermissionRoute>} />
-          <Route path="/admin/financeiro" element={<PermissionRoute permission="finance.view"><AdminFinance /></PermissionRoute>} />
+          <Route path="/admin/financeiro" element={<PermissionRoute permission="financial.view"><AdminFinance /></PermissionRoute>} />
         </Route>
       </Route>
     </Routes>
